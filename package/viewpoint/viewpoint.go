58c2d01b3a8cf0e12061c7cc58697d8a3a15e18c
sgallaghe1541 <sgallaghe1541@gmail.com>
2024-05-30 15:14:43 -0400

    baby steps

---
+++ package/viewpoint/viewpoint.go
@@ -0,0 +1,32 @@
+package viewpoint
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/microsoft/go-mssqldb"
+)
+
+func ConnectToViewpoint() (*sql.DB, error) {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	vpServer := os.Getenv("VP_SERVER")
+	vpDB := os.Getenv("VP_DATABASE")
+	vpUser := os.Getenv("VP_USER")
+	vpPass := os.Getenv("VP_PASSWORD")
+	vpPort := os.Getenv("VP_PORT")
+	connString := fmt.Sprintf("server=%s;database=%s;user id=%s;password=%s;port=%s", vpServer, vpDB, vpUser, vpPass, vpPort)
+
+	conn, err := sql.Open("mssql", connString)
+	if err != nil {
+		return nil, fmt.Errorf("viewpoint connection failed: %s", err.Error())
+	}
+	return conn, nil
+}

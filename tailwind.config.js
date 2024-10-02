5291f7dca376d26364e301ce041ba50e3ac340b7
sgallaghe1541 <sgallaghe1541@gmail.com>
2024-10-02 16:32:54 -0400

    grid works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.templ"],
+  content: ["./views/**/*.templ", "./internal/timeentry/*.templ"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'mainGrid': 'theme(space.24) 1fr'
+      },
+      gridTemplateRows: {
+        'mainGrid': '80px 1fr 32px'
+      }
+    },
   },
   plugins: [],
 }

d974fb031258bab7ea47f76341ca4e8434343f15
sgallaghe1541 <sgallaghe1541@gmail.com>
2024-10-09 14:07:41 -0400

    prettier and prettier

--- internal/timeentry/hyperscript.go
+++ internal/timeentry/hyperscript.go
@@ -3,29 +3,75 @@ package timeentry
 const (
 	phaseScript = `
 		on change 
-			if my value is not empty send phase%dActive to #hours then remove @disabled from next <select/> within #phases 
-			else send phase%dInactive to #hours then send clearPhase to the next <select/> within #phases 
+			if my value is not empty send phase%dActive to #hours 
+				if next <select/> within #phases != null remove @disabled from next <select/> end
+			else send phase%dInactive to #hours
+				if next <select/> within #phases != null send clearPhase to the next <select/> end
 		end 
-		on clearPhase set my value to empty add @disabled to me send phase%dInactive to #hours then send clearPhase to the next <select/> within #phases end
+		on clearPhase set my value to '' add @disabled to me then set @disabled to 'disabled' then send phase%dInactive to #hours
+			if next <select/> within #phases != null send clearPhase to the next <select/> end
+		end
 	`
 	phaseCountScript = `
 		on phase1Active from #hours or phase2Active from #hours or phase3Active from #hours or phase4Active from #hours or phase5Active from #hours or phase5Inactive from #hours
 			set :count to 0
 			set :allPhases to <select/> in #phases
 			for p in :allPhases
-				if p's value is not empty increment :count by 1
+				if p's value is not '' increment :count by 1
 			end
 			set my value to :count
 		end
 	`
 	employeeHoursScript = `
-		on phase%dActive from #hours remove @disabled from me end 
-		on phase%dInactive from #hours set my value to empty then add @disabled to me end
-		on jobSelected from #phases set my value to empty then add @disabled to me end
+		on phase%dActive from #hours set @phase-selected to 'y' then if @employee-selected == 'y' remove @disabled from me end 
+		on phase%dInactive from #hours set my value to null then add @disabled to me then set @disabled to 'disabled' end
+		on jobSelected from #phases 
+			set my value to null 
+			add @disabled to me 
+			set @disabled to 'disabled' 
+			set @phase-selected to 'n'
+		end
+		on employeeSelected from #hours 
+			if @phase-selected == 'y' and @employee-selected == 'y'
+				remove @disabled from me
+			end
+		end
 	`
 	equipmentHoursScript = `
-		on phase%dActive from #hours remove @disabled from me end 
-		on phase%dInactive from #hours set my value to empty then add @disabled to me end
-		on jobSelected from #phases set my value to empty then add @disabled to me end
+		on phase%dActive from #hours set @phase-selected to 'y' then if @equipment-selected == 'y' remove @disabled from me end 
+		on phase%dInactive from #hours set my value to null then add @disabled to me then set @disabled to 'disabled' end
+		on jobSelected from #phases 
+			set my value to null
+			add @disabled to me 
+			set @disabled to 'disabled' 
+			set @phase-selected to 'n'
+		end
+		on equipmentSelected from #hours 
+			if @phase-selected == 'y' and @equipment-selected == 'y'
+				remove @disabled from me
+			end
+		end
+	`
+	employeeSelectScript = `
+		on change 
+			if my value is not empty
+				set inputs to <input/> in closest <div/>
+				for i in inputs 
+					set i's @employee-selected to 'y'
+				end
+			end
+			send employeeSelected to #hours
+		end 
+	`
+	equipmentSelectScript = `
+		on change 
+			if my value is not empty
+				set inputs to <input/> in closest <div/>
+				for i in inputs 
+					set i's @equipment-selected to 'y'
+				end
+			end
+			send equipmentSelected to #hours
+		end 
 	`
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ module.exports = {
     extend: {
       gridTemplateColumns: {
         'main': 'theme(space.24) 1fr',
-        'timeEntry': 'theme(space.60) repeat(2, minmax(theme(space.20), theme(space.20)))  repeat(5, minmax(theme(space.52), 1fr)) theme(space.20)'
+        'timeEntry': 'theme(space.60) repeat(2, minmax(theme(space.20), theme(space.20))) repeat(5, minmax(theme(space.24), 1fr)) theme(space.20)'
       },
       gridTemplateRows: {
         'main': 'theme(space.20) calc(100vh - theme(space.8) - theme(space.20)) theme(space.8)',
-        'timeEntry': 'minmax(theme(space.16), max-content) minmax(theme(space.8), max-content) 1fr',
-        'hours': 'minmax(min-content, max-content) minmax(min-content, max-content)'
+        'timeEntry': 'minmax(min-content, max-content) 1fr',
+        'hours': 'repeat(4, minmax(min-content, max-content))'
       }
     },
   },
